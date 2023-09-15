5b02c35f998fb206a0723d8ccf20d8e3a2385adc
ESCristiano <cris96r@gmail.com>
2023-09-15 18:24:40 +0100

    feat: Add bao-demos test derivation
    Signed-off-by: ESCristiano <cris96r@gmail.com>

---
+++ pkgs/bao/bao_tf.nix
@@ -0,0 +1,53 @@
+{ stdenv
+, fetchFromGitHub
+, fetchurl
+, toolchain
+, python3
+, python3Packages
+, rsync
+, guest
+, demos
+, platform
+}:
+
+stdenv.mkDerivation rec {
+    # Derivation to build bao to run the bao test framework (as a guest)
+    # MUT: bao-hypervisor
+    pname = "bao-tf";
+    version = "1.0.0";
+
+    src = ../../../../.;
+    
+    nativeBuildInputs = [ toolchain guest demos]; #build time dependencies
+    buildInputs = [python3 python3Packages.numpy rsync];
+
+    unpackPhase = ''
+        mkdir -p $out
+        #copy everything except tests
+        rsync -r --exclude 'tests' $src/ $out 
+        cd $out
+    '';
+
+    buildPhase = ''
+        export ARCH=aarch64
+        export CROSS_COMPILE=aarch64-none-elf-
+        export DEMO=baremetal
+        mkdir -p ./config
+        cp -L ${demos}/demos/$DEMO/configs/${platform}.c \
+                ./config/$DEMO.c
+        mkdir -p ./$DEMO
+        cp -L ${guest}/bin/baremetal.bin ./$DEMO
+        make PLATFORM=${platform}\
+             CONFIG_REPO=./config\
+             CONFIG=$DEMO\
+             CPPFLAGS=-DBAO_DEMOS_WRKDIR_IMGS=./$DEMO
+    '';
+    
+    installPhase = ''
+        mkdir -p $out/bin
+        cp ./bin/${platform}/$DEMO/bao.bin $out/bin
+    '';
+
+}
+
+
