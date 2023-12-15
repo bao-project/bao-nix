4fb8676d2c3dd295f02c34357b276830d7fa89e3
Diogo Costa <diogoandreveigacosta@gmail.com>
2023-12-15 11:01:39 +0000

    fix(build): remove useless steps of baremetal-local-tf
    Signed-off-by: Diogo Costa <diogoandreveigacosta@gmail.com>

--- pkgs/guest/baremetal-local-tf.nix
+++ pkgs/guest/baremetal-local-tf.nix
@@ -38,18 +38,12 @@ stdenv.mkDerivation rec {
     '';
 
     buildPhase = ''
-        echo "Platform: ${platform}"
-        echo "Suites: ${list_suites}"
-        echo "Testes: ${list_tests}"
         export ARCH=${plat_arch}
         export CROSS_COMPILE=${plat_toolchain}
-        export TESTF_TESTS_DIR=$out/tests
-        export TESTF_REPO_DIR=$out/bao-tests
-        chmod -R u+w bao-tests #make sure we can write to bao-tests
-        cd bao-tests
-        python3 ./codegen.py -d $TESTF_TESTS_DIR -o $TESTF_REPO_DIR/src/testf_entry.c
-        cd ..
-        make PLATFORM=${platform} BAO_TEST=1 SUITES=${list_suites} TESTS=${list_tests}
+        export TESTF_TESTS_DIR=$out/tests/src
+        export TESTF_REPO_DIR=$out/tests/bao-tests
+        chmod -R u+w $out/tests/bao-tests
+        make -C $out PLATFORM=${platform} BAO_TEST=1 SUITES=${list_suites} TESTS=${list_tests}
     '';
     
     installPhase = ''
