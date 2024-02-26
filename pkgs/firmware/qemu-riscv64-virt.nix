1b7d57eb4710b9b1934a27f7fdac3c5478953f81
Miguel Silva <miguelafsilva5@gmail.com>
2024-02-26 15:25:18 +0000

    feat(firmware): Add recipe for qemu-riscv64-virt firmware
    This recipe assigns openSBI as its dependency forcing its build through
    its own recipe.
    During the installPhase, this recipe copies the binaries generated to proper
    directory.
    
    Signed-off-by: Miguel Silva <miguelafsilva5@gmail.com>

---
+++ pkgs/firmware/qemu-riscv64-virt.nix
@@ -0,0 +1,31 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (c) Bao Project and Contributors. All rights reserved.
+{ pkgs
+, stdenv
+, fetchFromGitHub
+, toolchain
+, platform
+}:
+let
+  sbi = pkgs.callPackage ./openSBI/openSBI.nix {
+    inherit toolchain;
+    inherit platform;
+  };
+in
+stdenv.mkDerivation rec {
+    pname = "qemu_riscv64_virt";
+    version = "firmware";
+
+    dontUnpack = true;
+    dontBuild = true;
+    
+    nativeBuildInputs = [
+        sbi
+    ];
+
+    installPhase = ''
+        mkdir -p $out
+        touch $out/test_derivation.txt
+        find ${sbi} -type f -exec cp {} $out \;
+    '';
+}
