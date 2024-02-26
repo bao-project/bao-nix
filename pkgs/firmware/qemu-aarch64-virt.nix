898a7a61f870f28324f779f857c424065f361298
Miguel Silva <miguelafsilva5@gmail.com>
2024-02-26 15:21:58 +0000

    feat(firmware): Add recipe for qemu-aarch64-virt firmware
    This recipe assigns atf and u-boot as its dependencies forcing their builds
    through their own recipes.
    During the installPhase, this recipe copies the binaries generated to proper
    directory.
    
    Signed-off-by: Miguel Silva <miguelafsilva5@gmail.com>

---
+++ pkgs/firmware/qemu-aarch64-virt.nix
@@ -0,0 +1,39 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (c) Bao Project and Contributors. All rights reserved.
+{ pkgs
+, stdenv
+, fetchFromGitHub
+, toolchain
+, platform
+}:
+let
+  u-boot = pkgs.callPackage ./u-boot/u-boot.nix 
+  {
+    inherit toolchain;
+  };
+
+  atf = pkgs.callPackage ./atf/atf.nix {
+    inherit toolchain;
+    inherit u-boot; 
+    inherit platform;
+  };
+in
+stdenv.mkDerivation rec {
+    pname = "qemu_aarch64_virt";
+    version = "firmware";
+
+    dontUnpack = true;
+    dontBuild = true;
+    
+    nativeBuildInputs = [
+        u-boot
+        atf
+    ];
+
+    installPhase = ''
+        mkdir -p $out
+        touch $out/test_derivation.txt
+        find ${atf} -type f -exec cp {} $out \;
+        find ${u-boot} -type f -exec cp {} $out \;
+    '';
+}
