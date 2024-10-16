4cf92fe1f960821be27b105209251dea8d113c61
Miguel Silva <miguelafsilva5@gmail.com>
2024-10-16 14:08:53 +0100

    feat(recipe): Add recipe for linux cross compile toolchain
    Signed-off-by: Miguel Silva <miguelafsilva5@gmail.com>

---
+++ pkgs/toolchains/aarch64-none-linux-gnu.nix
@@ -0,0 +1,40 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (c) Bao Project and Contributors. All rights reserved.
+
+{ lib
+, stdenv
+, fetchurl
+, ncurses5
+, python38
+}:
+
+stdenv.mkDerivation rec {
+  pname = "aarch64-none-linux-elf";
+  version = "13.2.rel1";
+  platform = "x86_64";
+
+  src = fetchurl {
+    url = "https://developer.arm.com/-/media/Files/downloads/gnu/${version}/binrel/arm-gnu-toolchain-${version}-${platform}-aarch64-none-linux-gnu.tar.xz";
+    sha256 = "sha256-EvzfE6dDBlUimyBDiknoVm4mVRugh1mSLNr0aVsNTiM=";
+  };
+
+  dontConfigure = true;
+  dontBuild = true;
+  dontPatchELF = true;
+  dontStrip = true;
+
+  installPhase = ''
+    mkdir -p $out
+    cp -r * $out
+  '';
+
+  # Package -> put the binaries with nix store paths
+  preFixup = ''
+    find $out -type f | while read f; do
+      patchelf "$f" > /dev/null 2>&1 || continue
+      patchelf --set-interpreter $(cat ${stdenv.cc}/nix-support/dynamic-linker) "$f" || true
+      patchelf --set-rpath ${lib.makeLibraryPath [ "$out" stdenv.cc.cc ncurses5 python38 ]} "$f" || true
+    done
+  '';
+
+}
