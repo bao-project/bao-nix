2064831f7103570c576ceaaf666cc2163c1bf3a3
Diogo Costa <diogoandreveigacosta@gmail.com>
2025-04-30 13:58:49 +0100

    fix(initramfs): update recipe to use pre-built images
    Signed-off-by: Diogo Costa <diogoandreveigacosta@gmail.com>

--- pkgs/guest/linux/initramfs.nix
+++ pkgs/guest/linux/initramfs.nix
@@ -2,6 +2,7 @@
 # Copyright (c) Bao Project and Contributors. All rights reserved.
 
 { stdenv
+, fetchurl
 , fetchgit
 , rsync
 , cpio
@@ -15,9 +16,9 @@ stdenv.mkDerivation rec {
     pname = "initramfs";
     version = "1.0.0";
 
-    linux_image = builtins.path {
-        #TODO: change cpio source url
-        path = /media/diogo/linux_rootfs_2/bao_dev/bao-linux-test/wrkdir/pack.tar.gz;
+    linux_image = fetchurl {
+        url = "https://github.com/bao-project/bao-linux-test/releases/download/v1.0.0-${setup-cfg.platform_name}/initramfs_${setup-cfg.platform_name}.tar.gz";
+        sha256 = "sha256-Rpe9phmrBJpmi/7sB3uPwxNRME+ZUtrGcMq/OvaULRc=";
     };
 
     buildInputs = [ cpio fakeroot ];
@@ -29,10 +30,12 @@ stdenv.mkDerivation rec {
     buildPhase = ''
         mkdir -p initramfs
         cd initramfs 
-        cpio -idm < ../rootfs.cpio || true
+        cpio -idm < ../wrkdir/rootfs_${setup-cfg.platform_name}.cpio || true
         chmod +x init bin/busybox sbin/init bin/sh
         mkdir -p home/testf
         cp ${linuxApp}/bin/program.out home/testf/testf-app.out
+        cp ${linuxApp}/bin/program.out etc/init.d/S99testf-app
+        chmod +x etc/init.d/S99testf-app
         find . | fakeroot cpio -o -H newc --owner root:root > ../new.cpio
         cd ..
     '';
