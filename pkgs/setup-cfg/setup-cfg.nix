859b2ecb327386f4305034648ac3697e29df017f
Diogo Costa <diogoandreveigacosta@gmail.com>
2025-07-10 16:30:31 +0100

    fix(setup_cfg): include risc-v linux toolchains
    Signed-off-by: Diogo Costa <diogoandreveigacosta@gmail.com>

--- pkgs/setup-cfg/setup-cfg.nix
+++ pkgs/setup-cfg/setup-cfg.nix
@@ -39,10 +39,26 @@ let
         fvp-r-aarch32 = "arm-none-eabi";
         qemu-riscv64-virt = "riscv64-unknown-elf";
     };
+
+    platforms-linux-toolchain = {
+        qemu-aarch64-virt = "aarch64-none-linux-gnu";
+        zcu102 = "aarch64-none-linux-gnu";
+        zcu104 = "aarch64-none-linux-gnu";
+        imx8qm = "aarch64-none-linux-gnu";
+        tx2 = "aarch64-none-linux-gnu";
+        rpi4 = "aarch64-none-linux-gnu";
+        fvp-a = "aarch64-none-linux-gnu";
+        fvp-r = "aarch64-none-linux-gnu";
+        fvp-a-aarch32 = "arm-none-eabi";
+        fvp-r-aarch32 = "arm-none-eabi";
+        qemu-riscv64-virt = "riscv64-unknown-linux-gnu";
+    };
+
 in {
     platform_name = "${platform}";
     arch = platforms-arch.${platform};
     toolchain_name = platforms-toolchain.${platform};
+    linux_toolchain_name = platforms-linux-toolchain.${platform};
     bao-tests = bao-tests;
     tests_srcs = tests_srcs;
     baremetal_patch = baremetal_patch;
