78649faaef749dc7d56bcbece80ec25da78fdd80
Diogo Costa <diogoandreveigacosta@gmail.com>
2025-11-11 20:02:24 +0000

    update(riscv_toolchain): update url to newer version
    Signed-off-by: Diogo Costa <diogoandreveigacosta@gmail.com>

--- pkgs/toolchains/riscv64-unknown-elf.nix
+++ pkgs/toolchains/riscv64-unknown-elf.nix
@@ -20,8 +20,8 @@ stdenv.mkDerivation rec {
   arch = "x86_64";
 
   src = fetchurl {
-    url = "https://github.com/bao-project/bao-riscv-toolchain/releases/download/gc891d8dc23e/riscv-unknown-elf-13.2.0-ubuntu-22.04.tar.gz";
-    sha256 = "sha256-MMKEFVwIl3wGwSZDHmvpVzEWsgpc6ePaX5kTrzyVfL0=";
+    url = "https://github.com/bao-project/bao-riscv-toolchain/releases/download/gc891d8dc23e/riscv64-unknown-elf.tar.gz";
+    sha256 = "sha256-aW74iTB3tza0KNn8M88hU/kcCTq3Bw5UN56L+wKpRdc=";
   };
 
   nativeBuildInputs = [ zlib ]; #build time dependencies
